635b5df6e2a9ff0c40c6e84432944b591f8fab1a
jaswantjs <jaswant@ryaz.io>
2023-08-05 22:26:03 +0530

    Feat: home left

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,20 +3,23 @@ export default function Home() {
         <div className="home">
             <div className="home-left">
                 <div className="home-left-title">
-                    <p>Hi all. I am</p>
-                    <h1>Jaswant Singh</h1>
-                    <p>{">"} Full Stack Developer</p>
+                    <p className="home-left-title-intro">Hi all. I am</p>
+                    <h1 className="home-left-title-name">Jaswant Singh</h1>
+                    <p className="home-left-title-position">{">"} Full Stack Developer</p>
                 </div>
                 <div className="home-left-code">
-                    <p>{"//"} complete the game to continue</p>
-                    <p>{"//"} you can also see it on my Github page</p>
-                    <div>
-                        <span>const</span>
-                        <span>githubLink</span>
-                        <span>=</span>
-                        <span>{"\"https://github.com/jaswantsinghh\""}</span>
+                    <p className="home-left-code-text">{"//"} complete the game to continue</p>
+                    <p className="home-left-code-text">{"//"} you can also see it on my Github page</p>
+                    <div className="home-left-code-github">
+                        <span className="home-left-code-github-const">const </span>
+                        <span className="home-left-code-github-variable">githubLink </span>
+                        <span className="home-left-code-github-equal">= </span>
+                        <span className="home-left-code-github-url">{"\"https://github.com/jaswantsinghh\""}</span>
                     </div>
                 </div>
+            </div>
+            <div className="home-right">
+
             </div>
         </div>
     )
